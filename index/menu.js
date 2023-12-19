d88b6b78edd6f2bbab9818f9cdded7e867da5a4b
Gabriel Patucci <patuccigabriel@gmail.com>
2023-12-19 17:00:23 -0300

    design responsivo

--- index/menu.js
+++ index/menu.js
@@ -1,4 +1,10 @@
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
     header.classList.toggle('rolagem', window.scrollY > 0)
-})
+});
+function clickMenu() {
+    
+    flexjs.style.display = 'flex'
+    header.style.height = '100vh'
+    buttonjs.style.display = 'none'
+}
